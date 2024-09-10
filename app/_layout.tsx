f5a66c38320017a757ebd3dd450a1197276ece71
Srinath10X <srinathcreates@gmail.com>
2024-09-10 10:39:18 +0530

    configured react-native-dotenv

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,12 @@
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from "@env";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
@@ -14,13 +23,13 @@ import { theme } from "@/theme";
 import { StatusBar } from "expo-status-bar";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoUHXtPYlGYrQyeKvZEpzGsZnOj1lD3Gk",
-  authDomain: "chat-app-bfdfd.firebaseapp.com",
-  projectId: "chat-app-bfdfd",
-  storageBucket: "chat-app-bfdfd.appspot.com",
-  messagingSenderId: "113641532071",
-  appId: "1:113641532071:web:69805890932d41642ec982",
-  measurementId: "G-F3SQFEJ81K",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
