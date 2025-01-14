9404574a9f71dbb9a6b7f3ebeae879db2d483627
Srinath10X <srinathcreates@gmail.com>
2025-01-14 21:06:29 +0530

    init: initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="font-bold">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+};
